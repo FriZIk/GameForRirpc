72fdfb0bf810c1ce2264016febbc0945c8aa1004
FriZIk <tester251100@gmail.com>
2020-06-30 01:45:18 +0300

    30.06.2020

---
+++ Отрисовка растительности/Assets/Source/CharacterController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float Speed;
+
+    public GameObject Menu;
+    bool Triger = false;
+
+    public void ContinueFu()
+    {
+        Menu.SetActive(false);
+    }
+
+    public void ExitToWindows()
+    {
+        Application.Quit();
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + Vector3.forward * Speed;
+            Debug.Log("Вперёд!");
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Влево!"); 
+            this.transform.position = transform.position + Vector3.left * Speed;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("Назад!"); 
+            transform.position = transform.position + Vector3.back * Speed;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Вправо!");
+            transform.position = transform.position + Vector3.right * Speed;
+        }
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Пауза");
+            if(Triger == true)
+            {
+                Menu.gameObject.SetActive(false); 
+                Triger = false;
+            }
+            else
+            { 
+                Menu.gameObject.SetActive(true); 
+                Triger = true;
+            }
+        }
+    }
+}
---
+++ Отрисовка растительности/Assets/Source/Generator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+
+    // Класс для координат
+    public class Point
+    {
+        public float X,Z;
+        private float Y  = 0.5f;
+
+        public void GenerateTerrain(GameObject Vegetable)
+        {   
+            Instantiate(Vegetable, new Vector3(X, Y, Z), Quaternion.identity);
+        }
+
+        // Конструктор класса
+        public Point(float X_Coordinate,float Z_Coordinate)
+        {
+            X = X_Coordinate;
+            Z = Z_Coordinate;
+        }
+    }
+
+    // Необходимые префабы для генерации
+    public GameObject Tree_1,Tree_2,Bush,Grass;
+    public List <Point> Vegetables = new List<Point>();
+
+    void Start()
+    {
+        int NumberOfVegetables = 50;
+        for(int i = 0;i < NumberOfVegetables;i++)
+        {
+            Vegetables.Add(new Point(Random.Range(1,100),Random.Range(1,70)));
+        }
+
+
+        // Генерация карты
+        foreach(Point p in Vegetables)
+        {
+            int KindOfVegetables = Random.Range(1,4);
+            switch(KindOfVegetables)
+            {
+                case 1:p.GenerateTerrain(Tree_1);break;
+                case 2:p.GenerateTerrain(Tree_2);break;
+                case 3:p.GenerateTerrain(Bush);break;
+                case 4:p.GenerateTerrain(Grass);break;
+            }
+        }
+    }
+}
